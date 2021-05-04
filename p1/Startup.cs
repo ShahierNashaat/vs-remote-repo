fd7b8f3910821a6a86fdfdb0b1418f23ecd49dfc
shahiern@yahoo.com <shahiern@yahoo.com>
2021-05-04 13:07:46 +0200

    master commit

--- p1/Startup.cs
+++ p1/Startup.cs
@@ -32,8 +32,7 @@ namespace p1
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello from first branch");
-                    await context.Response.WriteAsync("Hello from first branch again");
+                    await context.Response.WriteAsync("Hello from master");
                 });
             });
         }
